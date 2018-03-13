d2e0cc7ade22df81f95467a641a7bf1a991b353b
santhoshraddp <E1>
2018-03-13 12:31:03 +0530

    ommit

---
+++ SI273181/execute.bat
@@ -0,0 +1,15 @@
+cd C:\Users\SA389141\Desktop\BasicExpressnode\downloads\SI273181
+git
+git init
+git config --global user.name "santhoshraddp"
+git config --global user.email "E1"
+git config --global credential.helper wincred
+git add SI273181
+git commit -m "  ommit"
+git remote -v
+git remote set-url origin https://santhoshraddp:Santhosh123@github.com/santhoshraddp/priya.git
+git remote add origin https://github.com/santhoshraddp/priya.git
+git push -u origin   master
+echo Press Enter...
+pause
+
